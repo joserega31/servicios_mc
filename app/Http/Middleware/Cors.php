0693ad5392efaea5732ae5a9cd71647405508ff5
joserega31 <joserega31@gmail.com>
2020-12-23 13:31:25 -0500

    .

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -6,11 +6,8 @@ class Cors
   public function handle($request, Closure $next)
   {
     return $next($request)
-       //Url a la que se le dará acceso en las peticiones
       ->header('Access-Control-Allow-Origin: *')
-      //Métodos que a los que se da acceso
-      ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OP")
-      //Headers de la petición
+      ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
       ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization"); 
   }
 }
